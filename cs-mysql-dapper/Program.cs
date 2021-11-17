0bcddedfd6e031c5097e555f43b343951d43dcc7
Nikita C <cethrivias@gmail.com>
2021-11-17 13:37:39 +0300

    Init commit

---
+++ cs-mysql-dapper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using MySqlConnector;
+
+namespace CSMysqlDapper
+{
+  public class CustomDbResponse
+  {
+    public string Text { get; set; }
+  }
+
+  class Program
+  {
+    private const string HOST = "127.0.0.1";
+    private const string USER = "root";
+    private const string PASSWORD = "testdbpass";
+    private const string DATABASE = "validator";
+
+    static async Task Main(string[] args)
+    {
+      var connectionString = $"Server={HOST};User ID={USER};Password={PASSWORD};Database={DATABASE}";
+
+      using (var connection = new MySqlConnection(connectionString)) {
+        connection.Open();
+
+        var text = (await connection.QueryAsync<CustomDbResponse>(
+          "SELECT @Text as text;", new { Text = "Hello, World!" }
+        )).First();
+
+        Console.WriteLine(text.Text);
+      }
+    }
+  }
+}
